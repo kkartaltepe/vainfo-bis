f3ab38c6547f5f1d72167289505f36127e352314
Kurt Kartaltepe <kkartaltepe@gmail.com>
2018-09-15 21:06:19 -0500

    Dumb vainfo tool

---
+++ build.rs
@@ -0,0 +1,34 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    println!("cargo:rustc-link-lib=va");
+    println!("cargo:rustc-link-lib=va-drm");
+    println!("cargo:rustc-link-search=/usr/lib/{}", target);
+
+    let gen_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let bindings_file = gen_path.join("bindings.rs");
+
+    let stat = std::fs::metadata(&bindings_file);
+    match stat.err() {
+        Some(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                let bindings = bindgen::Builder::default()
+                    .header("/usr/include/va/va.h")
+                    .header("/usr/include/va/va_drm.h")
+                    .header("/usr/include/va/va_drmcommon.h")
+                    .generate()
+                    .expect("Unable to generate bindings");
+
+                bindings
+                    .write_to_file(&bindings_file)
+                    .expect("Couldnt write bindings!");
+            },
+            _ => {},
+        },
+        None => {},
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,146 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[macro_use]
+extern crate error_chain;
+
+mod errors {
+    error_chain!{
+        foreign_links {
+            Io(::std::io::Error);
+            Str(::std::str::Utf8Error);
+        }
+    }
+}
+
+use errors::*;
+use VAProfile::*;
+use VAEntrypoint::*;
+use VAConfigAttribType::*;
+use VASurfaceAttribType::*;
+
+use std::os::unix::io::AsRawFd;
+
+fn fourcc<'a>(code: &'a u64) -> Result<&'a str> {
+    let arr = unsafe { std::mem::transmute::<&u64, &[u8; 8]>(code) };
+    let lower = &arr[0..4];
+    return Ok(std::str::from_utf8(lower)?);
+
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        bail!("You must pass the DRM device");
+    }
+    let device = std::fs::File::open(&args[1])?;
+
+    let display = unsafe { vaGetDisplayDRM(device.as_raw_fd()) };
+    if display == std::ptr::null_mut() {
+        bail!(format!("Failed to open display on {:?}", device))
+    }
+
+    let mut vaapi_maj = 0;
+    let mut vaapi_min = 0;
+    let mut status = unsafe { vaInitialize(display, &mut vaapi_maj, &mut vaapi_min) };
+    if status == VA_STATUS_SUCCESS as i32 {
+        println!("VA-API version {}.{} initialized", vaapi_maj, vaapi_min);
+    } else {
+        bail!(format!("Failed to initialize VA-API on DRM display: {:?}", device));
+    }
+
+    let mut num_profiles = unsafe { vaMaxNumProfiles(display) };
+    let mut profiles = vec![VAProfileNone; num_profiles as usize];
+    status = unsafe { vaQueryConfigProfiles(display, profiles.as_mut_ptr(), &mut num_profiles) };
+    if status != VA_STATUS_SUCCESS as i32 {
+        bail!(format!("vaQueryConfigProfiles failed"));
+    }
+    profiles.retain(|&p| {
+        if p == VAProfileNone { return false };
+        let mut num_entrypoints = unsafe { vaMaxNumEntrypoints(display) };
+        // Added into enum as val 0
+        let mut entrypoints = vec![VAEntrypointNone; num_entrypoints as usize];
+        status = unsafe { vaQueryConfigEntrypoints(display,
+                                                      p,
+                                                      entrypoints.as_mut_ptr(),
+                                                      &mut num_entrypoints) };
+        if status == VA_STATUS_ERROR_UNSUPPORTED_PROFILE as i32 {
+            return false;
+        }
+        entrypoints.retain(|&e| 
+            e == VAEntrypointEncSlice ||
+            e == VAEntrypointEncSliceLP
+        );
+        if entrypoints.len() > 0 {
+            println!("{:?}:", p);
+        }
+
+        for e in entrypoints.iter() {
+            let mut attrib_list = vec![
+                VAConfigAttrib{type_: VAConfigAttribEncMaxRefFrames, value: VA_ATTRIB_NOT_SUPPORTED},
+                VAConfigAttrib{type_: VAConfigAttribMaxPictureWidth, value: VA_ATTRIB_NOT_SUPPORTED},
+                VAConfigAttrib{type_: VAConfigAttribMaxPictureHeight, value: VA_ATTRIB_NOT_SUPPORTED},
+                VAConfigAttrib{type_: VAConfigAttribEncQualityRange, value: VA_ATTRIB_NOT_SUPPORTED},
+                VAConfigAttrib{type_: VAConfigAttribEncPackedHeaders, value: VA_ATTRIB_NOT_SUPPORTED},
+                VAConfigAttrib{type_: VAConfigAttribEncQuantization, value: VA_ATTRIB_NOT_SUPPORTED},
+                VAConfigAttrib{type_: VAConfigAttribEncMacroblockInfo, value: VA_ATTRIB_NOT_SUPPORTED},
+            ];
+            status = unsafe { vaGetConfigAttributes(display,
+                                          p,
+                                          *e,
+                                          attrib_list.as_mut_ptr(),
+                                          attrib_list.len() as i32) };
+            if status != VA_STATUS_SUCCESS as i32 {
+                return false;
+            }
+            let mut config = 0 as VAConfigID;
+            let status = unsafe { vaCreateConfig(display, p, *e, std::ptr::null_mut(), 0, &mut config) };
+            if status != VA_STATUS_SUCCESS as i32 || config == 0 as VAConfigID {
+                return false;
+            }
+
+            let mut num_surf_attribs = 0;
+            let status = unsafe { vaQuerySurfaceAttributes(display, config, std::ptr::null_mut(), &mut num_surf_attribs) };
+            if status != VA_STATUS_SUCCESS as i32 {
+                println!("Failed to query surface attributes size");
+                return false
+            }
+            let mut surf_attribs = unsafe { vec![ std::mem::zeroed::<VASurfaceAttrib>(); num_surf_attribs as usize] };
+            let status = unsafe { vaQuerySurfaceAttributes(display,
+                                                          config,
+                                                          surf_attribs.as_mut_ptr(),
+                                                          &mut num_surf_attribs) };
+            if status != VA_STATUS_SUCCESS as i32 {
+                println!("Failed to query surface attributes");
+                return false
+            }
+            println!("\t{:?}:", e);
+            surf_attribs.retain(|attrib| return attrib.type_ != VASurfaceAttribNone);
+            attrib_list.retain(|attrib| return attrib.value != VA_ATTRIB_NOT_SUPPORTED);
+            for a in attrib_list.iter() {
+                if a.type_ == VAConfigAttribEncMaxRefFrames {
+                    println!("\t\t{:?}: P-Frames: {}, B-Frames: {}", 
+                             a.type_,
+                             a.value & 0xFFFF,
+                             (a.value >> 16) & 0xFFFF);
+                } else {
+                    println!("\t\t{:?}:{}", a.type_, a.value);
+                }
+            }
+            for s in surf_attribs.iter() {
+                if s.type_ == VASurfaceAttribPixelFormat {
+                    println!("\t\t{:?}:{:?}", s.type_, fourcc(&s.value.value.bindgen_union_field));
+                } else if s.type_ == VASurfaceAttribMemoryType {
+                    println!("\t\t{:?}:0b{:b}", s.type_, s.value.value.bindgen_union_field as u32);
+                } else {
+                    println!("\t\t{:?}:{:?}", s.type_, s.value.value.bindgen_union_field);
+                }
+            }
+        }
+        return true;
+    });
+
+    Ok(())
+}
